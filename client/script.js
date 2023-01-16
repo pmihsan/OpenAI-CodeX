4d75edb401cd297d15864d9e0f8a0769b49897a5
Mohamed Ihsan <89512452+pmihsan@users.noreply.github.com>
2023-01-16 17:55:03 +0530

    Updated Server URL

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async(e) => {
     loader(messageDiv);
 
     // Fetch data from the server
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://openai-codex-bot.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
